7874b9208abf8039b0179d25d362235e4fd9e4b6
Ilia Kamilov <iliakmlv@gmail.com>
2025-02-19 18:53:55 +0200

    feat(layout): added footer and updated layout structure
    - updated `src/app/globals.css` to contain all the elements with relative layout
    - updated `src/app/layout.tsx` to include the new footer component
    - added `src/components/layout/Footer.tsx` with copyrights text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,7 +23,8 @@ export default function RootLayout({
     <html lang="he">
       <body dir="rtl" className={`${inter.className} antialiased`}>
         <Navbar />
-        <main>{children}</main>
+        <main className="h-full">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,21 @@
+/**
+ * @file src\components\layout\Footer.tsx
+ * @description Main footer component
+ * @author Ilia Kamilov <iliakmlv@gmail.com> (https://github.com/iliakamilov)
+ * @date 19/02/2025
+ * @license MIT
+ * @version 1.0.0
+ */
+
+const Footer = () => {
+  return (
+    <footer
+      dir="ltr"
+      className="flex justify-center items-center p-4 text-gray-300"
+    >
+      Synctip &copy; 2025
+    </footer>
+  );
+};
+
+export default Footer;
