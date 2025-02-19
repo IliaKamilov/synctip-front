417428cb9f14a47eabca93ee56bb92183cfff244
Ilia Kamilov <iliakmlv@gmail.com>
2025-02-19 04:21:47 +0200

    feat(navbar): initial commit

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+/**
+ * @file src\components\layout\Navbar.tsx
+ * @description The main navigation component for the application
+ * @author Ilia Kamilov <iliakmlv@gmail.com> (https://github.com/iliakamilov)
+ * @date 19/02/2025
+ * @license MIT
+ * @version 1.0.0
+ */
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
