0729ea84b8c0edad00ec17cc9f5746c7f1a16a11
Ilia Kamilov <iliakmlv@gmail.com>
2025-02-21 10:08:31 +0200

    feat: added input and form, sync package with git version
    - **ci(release.yml):** updated workflow to sync `package.json` version with Git tag from Semantic Release
    - **feat(ui):** added `Form` component with styles for text input fields
    - **feat(ui):** added `TextField` component with styles for consistent input handling
    - **chore(page.tsx):** updated main page to display a form for development purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Form from "@/components/form/Form";
+import TextField from "@/components/input/TextField";
+
 const Home = () => {
-  return <div>Page</div>;
+  return (
+    <div className="container sm:p-2 p-4 w-full mx-auto">
+      <Form>
+        <TextField type="date" placeholder="בחר תאריך" label="תאריך" />
+        <TextField type="number" placeholder="סכום טיפים" label="טיפים" />
+        <TextField type="number" placeholder="סכום קופה" label="קופה" />
+      </Form>
+    </div>
+  );
 };
 
 export default Home;
