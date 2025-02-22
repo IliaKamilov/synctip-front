3e9003f1775bb50a1fc3236e39d24e1af78e4847
Ilia Kamilov <iliakmlv@gmail.com>
2025-02-22 08:18:23 +0200

    fix: resolve issues in last run
    - chore: update jest.config.ts to ignore Playwright test files
    - deps: install missing @types/jest for Jest typings
    - fix: update package-lock.json and package.json for missing dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -164,15 +164,15 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    "\\\\node_modules\\\\",
+    "/tests/playwright/",
+    "tests-examples",
+    "e2e",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
