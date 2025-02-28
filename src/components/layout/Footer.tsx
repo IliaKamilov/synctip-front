9e5a66103520a76c750667147f0fc86dedcab3f6
Ilia Kamilov <iliakmlv@gmail.com>
2025-02-28 14:35:00 +0200

    feat(footer): improve styles with Tailwind CSS

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,15 +7,28 @@
  * @version 1.0.0
  */
 
+import Link from "next/link";
 import pkg from "../../../package.json";
 
 const Footer = () => {
   return (
     <footer
       dir="ltr"
-      className="select-none flex text-xs justify-center items-center text-gray-300 dark:text-gray-600"
+      className="select-none flex justify-center items-center py-6 text-xs font-light text-gray-400 hover:text-gray-500 dark:text-gray-600 dark:hover:text-gray-500 transition-colors duration-300"
     >
-      synctip {pkg.version} &copy; 2025
+      <p className="flex items-center space-x-2">
+        <Link href="/" className="hover:text-gray-600 transition-colors">
+          synctip
+        </Link>
+        <Link
+          href={`https://github.com/IliaKamilov/synctip/releases/tag/v${pkg.version}`}
+          className="hover:text-gray-600 transition-colors"
+        >
+          v{pkg.version}
+        </Link>
+        <span className="mx-1.5 text-gray-500">&copy;</span>
+        <span className="font-medium text-gray-500">2025</span>
+      </p>
     </footer>
   );
 };
